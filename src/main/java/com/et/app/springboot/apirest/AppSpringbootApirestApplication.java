6f7d29bb70531f5c823ec815683b960502308bdd
alejandroq <alejandrodebian25@gmail.com>
2022-07-12 10:33:56 -0400

    Inicio del proyecto

---
+++ src/main/java/com/et/app/springboot/apirest/AppSpringbootApirestApplication.java
@@ -0,0 +1,13 @@
+package com.et.app.springboot.apirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppSpringbootApirestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppSpringbootApirestApplication.class, args);
+	}
+
+}
