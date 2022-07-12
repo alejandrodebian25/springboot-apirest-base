152014cba44a16be2b10c8c2bca86691e51a4d98
alejandroq <alejandrodebian25@gmail.com>
2022-07-12 11:14:02 -0400

    Entity. Relacion OneToOne

---
+++ src/main/java/com/et/app/springboot/apirest/entity/Direccion.java
@@ -0,0 +1,35 @@
+package com.et.app.springboot.apirest.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Entity
+@Table(name = "direcciones")
+@Data
+public class Direccion implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "direccion_id")
+    private Long direccionId;
+
+    private String numero;
+
+    private String calle;
+
+    private String zona;
+
+    @OneToOne(mappedBy = "direccion")
+    private Proveedor proveedor;
+}
---
+++ src/main/java/com/et/app/springboot/apirest/entity/Proveedor.java
@@ -0,0 +1,39 @@
+package com.et.app.springboot.apirest.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Entity
+@Table(name = "proveedores")
+@Data
+public class Proveedor implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "proveedor_id")
+    private Long proveedorId;
+
+    @Column(name = "nit_proveedor", unique = true)
+    private String nitProveedor;
+
+    @Column(name = "nombre_proveedor")
+    private String nombreProveedor;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "direccion_id", referencedColumnName = "direccion_id")
+    private Direccion direccion;
+
+}
